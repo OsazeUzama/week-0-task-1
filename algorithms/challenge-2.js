a9e2933349ab752c0d935831f51c2d980db98d39
OsazeUzama <Osazeuzama@gmail.com>
2023-10-17 04:10:37 +0100

    First commit

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,11 @@
-function checkNumber(num) {}
+function checkNumber(num) {
+    if (num > 0) {
+        return "Positive";
+    } else if (num < 0) {
+        return "Negative";
+    } else {
+        return "Zero";
+    }
+}
 
 module.exports = checkNumber;
